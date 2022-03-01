ff8caa637ec37dacb413b1cc6e70afcb05d5b4b6
Dustin Holden <dustin@devholden.com>
2022-03-01 01:31:19 -0500

    Code cleanup

--- src/Version.cpp
+++ src/Version.cpp
@@ -17,11 +17,11 @@ bool findKernelPatchVersion(uint8_t *version) {
 
     for(uint16_t offset = 0; offset < 0x00004000; offset++) {
         if(memcmp(tag, findStartAddress + offset, sizeof(tag) - 1) == 0) {
-        version[0] = (findStartAddress + offset)[6];
-        version[1] = (findStartAddress + offset)[7];
-        version[2] = (findStartAddress + offset)[8];
+            version[0] = (findStartAddress + offset)[6];
+            version[1] = (findStartAddress + offset)[7];
+            version[2] = (findStartAddress + offset)[8];
 
-        return true;
+            return true;
         }
     }
 
