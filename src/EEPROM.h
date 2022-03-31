5943ec8a348108d3747bee8c2f284fe5f1960b23
Dustin Holden <dustin@devholden.com>
2022-03-31 19:17:31 -0400

    Code cleanup

--- src/EEPROM.h
+++ src/EEPROM.h
@@ -5,53 +5,6 @@
 
 #define EEPROM_PATH "C:\\xboxhd\\eeprom.bin"
 
-enum class OUTPUT_RESOLUTION {
-    C480P = 0,
-    C720P
-};
-
-enum class OUTPUT_WIDESCREEN {
-    AUTO = 0,
-    FORCE,
-    NEVER
-};
-
-enum class AUDIO_COMP {
-    COMPAT = 0,
-    AUTO
-};
-
-enum class COLORSPACE {
-    AUTO = 0,
-    YCBCR,
-    RGB
-};
-
-enum class INTERPOLATION {
-    BILINEAR = 0,
-    TRUNC
-};
-
-enum class INTERPOLATION_X_SCALE {
-    BILINEAR = 0,
-    TRUNC
-};
-
-enum class INTERPOLATION_Y_SCALE {
-    BILINEAR = 0,
-    TRUNC
-};
-
-enum class INTERPOLATION_X_WEIGHT {
-    ROUND = 0,
-    TRUNC
-};
-
-enum class INTERPOLATION_Y_WEIGHT {
-    ROUND = 0,
-    TRUNC
-};
-
 typedef struct EEPROM_T {
     uint8_t  firmware_version[4];
     uint8_t  software_version[4];
